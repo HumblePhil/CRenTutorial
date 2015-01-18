6d9a434285eab7d9ff03fd79d98d9cf4b8fb1abd
Phil <phil.li@udngroup.com.tw>
2015-01-18 23:59:45 +0800

    Add Test class

---
+++ src/test/java/udndigital/test/FirstTest.java
@@ -0,0 +1,16 @@
+package udndigital.test;
+
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class FirstTest extends TestCase {
+	
+	//@Test
+    public void testZero() {
+        assertEquals(0, 0);
+    }
+}
